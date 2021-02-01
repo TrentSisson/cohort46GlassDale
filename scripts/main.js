82e5f6039c5225704c41cbcb6e2247dd4204144a
TrentSisson <trent.c.sisson0@gmail.com>
2021-02-01 10:36:18 -0600

    filtered criminals by arresting officers

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { OfficerList } from "./officers/OfficerList.js"
+import { OfficerSelect } from "./officers/OfficerSelect.js"
 
 
 CriminalList()
 OfficerList()
 ConvictionSelect()
+OfficerSelect()

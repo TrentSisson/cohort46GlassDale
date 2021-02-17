56108b0e5cfcac82701da7bf32984a3d332504d4
TrentSisson <trent.c.sisson0@gmail.com>
2021-02-17 11:09:28 -0600

    created a note form and able to save notes in localdb

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 import { CriminalList } from "./criminals/CriminalList.js"
+import { NoteForm } from "./notes/NoteForm.js"
 import { OfficerList } from "./officers/OfficerList.js"
 import { OfficerSelect } from "./officers/OfficerSelect.js"
+import "./notes/NoteList.js"
 
 
 CriminalList()
 OfficerList()
 ConvictionSelect()
 OfficerSelect()
+NoteForm()
